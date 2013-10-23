1160675907a20382a74f830f47137571a57727b5
Andrew Pearson-Wood <andrewpw133@gmail.com>
2013-10-23 04:15:17 -1000

    Wod 4

---
+++ app/Global.java
@@ -0,0 +1,24 @@
+import models.Contact;
+import models.ContactDB;
+import play.Application;
+import play.GlobalSettings;
+import views.formdata.ContactFormData;
+
+public class Global extends GlobalSettings {
+  
+  public void onStart(Application app) {
+    Contact contact = new Contact("Bob", "Barker", "555-555-5555", 1, "Mobile");
+    Contact contact2 = new Contact("Bill", "Murray", "555-555-5555", 2, "Home");
+    Contact contact3 = new Contact("Bo", "Jackson", "555-555-5555", 3, "Work");
+    Contact contact4 = new Contact("Babe", "Ruth", "555-555-5555", 4, "Mobile");
+    ContactFormData data = new ContactFormData(contact);
+    ContactFormData data2 = new ContactFormData(contact2);
+    ContactFormData data3 = new ContactFormData(contact3);
+    ContactFormData data4 = new ContactFormData(contact4);
+    ContactDB.add(data);
+    ContactDB.add(data2);
+    ContactDB.add(data3);
+    ContactDB.add(data4);
+  }
+
+}
